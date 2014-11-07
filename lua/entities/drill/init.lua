ec9c38ac90550342163dd45b9a4bb95f546d0352
Adam Wilson <adamwilson790@aol.co.uk>
2014-11-07 02:13:45 +0000

    Initial commit

---
+++ lua/entities/drill/init.lua
@@ -0,0 +1,23 @@
+AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
+AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
+ 
+include('shared.lua')
+ 
+function ENT:Initialize()
+ 
+	self:SetModel( "models/props_lab/tpplug.mdl" )
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	-- disable the movement of the entity
+	self:SetMoveType( MOVETYPE_NONE )
+	
+	self:SetUseType( SIMPLE_USE )
+	
+end
+ 
+function ENT:Use( activator, caller )
+end
+
+function ENT:Think()
+end
+
---
+++ lua/entities/drill/shared.lua
@@ -0,0 +1,16 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName= "Drill"
+ENT.Author= "Adzter"
+ENT.Contact= "http://steamcommunity.com/id/imadzter"
+ENT.Purpose= ""
+ENT.Instructions= ""
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
+
+drillConfig = {}
+
+-- You should only really edit the values below
+-- here unless you know what you're doing
+
