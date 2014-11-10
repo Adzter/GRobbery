f542290d1ff48cef2d25d9213eb4d64a93a2aec5
Adam Wilson <adamwilson790@aol.co.uk>
2014-11-10 23:40:26 +0000

    Setup the config file ready for use

---
+++ lua/autorun/server/config.lua
@@ -0,0 +1,37 @@
+AddCSLuaFile()
+drillConfig = {}
+cabinetConfig = {}
+
+--[[
+  _____    _____    _____   _        _      
+ |  __ \  |  __ \  |_   _| | |      | |     
+ | |  | | | |__) |   | |   | |      | |     
+ | |  | | |  _  /    | |   | |      | |     
+ | |__| | | | \ \   _| |_  | |____  | |____ 
+ |_____/  |_|  \_\ |_____| |______| |______|
+                                            
+--]]
+
+drillConfig.duration = 360
+--With the above config you can also use math.Randomnto specify a 
+--random value to drill for, example being math.random( 240, 360 )
+ 
+drillConfig.enableJamming = true -- Enables/disables the jamming of the drill
+drillConfig.drillingText = "DRILLING" -- Text to show when drilling
+drillConfig.errorText = "ERROR: JAM"  -- Text to show when the drill gets stuck
+
+--[[
+   _____              ____    _____   _   _   ______   _______ 
+  / ____|     /\     |  _ \  |_   _| | \ | | |  ____| |__   __|
+ | |         /  \    | |_) |   | |   |  \| | | |__       | |   
+ | |        / /\ \   |  _ <    | |   | . ` | |  __|      | |   
+ | |____   / ____ \  | |_) |  _| |_  | |\  | | |____     | |   
+  \_____| /_/    \_\ |____/  |_____| |_| \_| |______|    |_|   
+                                                               
+--]]
+
+cabinetConfig.rewards = {
+	-- Entity name, then quantity
+	{ "m9k_ak47", 2 },
+	{ "weapon_crowbar", 4}
+}
--- lua/entities/drill/shared.lua
+++ lua/entities/drill/shared.lua
@@ -8,8 +8,3 @@ ENT.Purpose= ""
 ENT.Instructions= ""
 ENT.Spawnable = true
 ENT.AdminSpawnable = false
-
-drillConfig = {}
-
--- You should only really edit the values below
--- here unless you know what you're doing
